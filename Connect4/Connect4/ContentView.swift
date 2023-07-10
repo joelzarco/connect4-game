332638b72adc274c6f0c88ea3b57b7558bd862a5
JohelCZarco <101434425+joelzarco@users.noreply.github.com>
2023-07-10 14:39:20 -0600

    s4: lower game turns ui block created

--- Connect4/Connect4/ContentView.swift
+++ Connect4/Connect4/ContentView.swift
@@ -105,18 +105,35 @@ struct ContentView: View {
                     .cornerRadius(15)
                     .padding(.bottom, 10)
                     
-                    HStack{
-                        // lower buttons here
-                    }
+                    HStack{// lower turn buttons here
+                        Spacer()
+                        switch turn {
+                        case .user:
+                            Text("Your turn")
+                                .font(.title.bold())
+                        case .computer:
+                            Text("Computer's turn")
+                                .font(.title.bold())
+                        case .userWin:
+                            Text("You won!")
+                                .font(.title.bold())
+                        case .computerWin:
+                            Text("You lost!!!")
+                                .font(.title.bold())
+                        case .tie:
+                            Text("No one wins :(")
+                                .font(.title.bold())
+                        }
+                        Spacer()
+                    } // hs
+                    .padding(8)
+                    .background(.blue.opacity(0.7))
+                    .cornerRadius(15)
+                    .padding(.bottom, 10)
+                    
                 } //vs
             } // nav
             
         } // geo
     }
 }
-
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
